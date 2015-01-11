4667cf546646f709e5ecc8c83173686e28851e75
Mateusz Szczap <mateusz.szczap@gmail.com>
2015-01-11 22:03:46 +0100

    active delegate packages

--- modules/closure-templates/app/soy/Soy.java
+++ modules/closure-templates/app/soy/Soy.java
@@ -7,7 +7,10 @@ import play.mvc.Controller;
 import play.twirl.api.Html;
 
 import javax.inject.Inject;
+import java.util.Arrays;
 import java.util.Locale;
+import java.util.Optional;
+import java.util.Set;
 
 public class Soy extends Controller {
 
@@ -19,17 +22,29 @@ public class Soy extends Controller {
     }
 
     public Html html(final String templateName, final SoyRecord model) {
-        return Html.apply(soyRenderer.render(templateName, locale(), request(), response(), model, ImmutableSet.of()));
+        final Set<String> activeDelegatePackages = activeDelegatePackages();
+
+        return Html.apply(soyRenderer.render(templateName, locale(), request(), response(), model, activeDelegatePackages));
     }
 
     public Html html(final String templateName) {
-        return Html.apply(soyRenderer.render(templateName, locale(), request(), response(), new SoyMapData()));
+        final Set<String> activeDelegatePackages = activeDelegatePackages();
+
+        return Html.apply(soyRenderer.render(templateName, locale(), request(), response(), new SoyMapData(), activeDelegatePackages));
     }
 
     private Locale locale() {
         return request().acceptLanguages().stream()
                 .findFirst().map(lang -> new Locale(lang.language()))
                 .orElse(Locale.ENGLISH);
+    }
+
+    private Set<String> activeDelegatePackages() {
+        return Optional.ofNullable(request().getQueryString("soyActivePackages"))
+                .map(str -> str.split(","))
+                .map(values -> Arrays.asList((values)))
+                .map(list -> ImmutableSet.<String>copyOf(list))
+                .orElse(ImmutableSet.of());
     }
 
 }
